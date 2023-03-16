74f36dcc36e4f6ff0866b8ca38da310d3921f3fa
aallyn <andrew.allyn@gmail.com>
2023-03-16 19:49:58 -0400

    project_model_aja checked and working

--- R/vast_functions.R
+++ R/vast_functions.R
@@ -1850,11 +1850,6 @@ project_model_aja<- function (x, what, n_proj = NULL, n_samples, uncert_res, new
             "Lat", "Region", "Area_km2", "STRATA")]
         spatial_list = proj_spatial
     }
-
-    # # Creating some necessary objects
-    # n_proj = max(new_covariate_data$Year) - max(x$covariate_data$Year)
-    # extrapolation_list = x$extrapolation_list
-    # input_grid = x$extrapolation_list$Data_Extrap
     reload_model(x)
     Obj = x$tmb_list$Obj
     Sdreport = x$parameter_estimates$SD
@@ -1864,10 +1859,10 @@ project_model_aja<- function (x, what, n_proj = NULL, n_samples, uncert_res, new
         if (!all(colnames(x$covariate_data) %in% colnames(new_covariate_data))) {
             stop("Please ensure that all columns of `x$covariate_data` are present in `new_covariate_data`")
         }
-        # n_proj_obs = nrow(new_covariate_data)
-        # t_proj = new_covariate_data$Year
-        # lat_proj = new_covariate_data$Lat
-        # lon_proj = new_covariate_data$Lon
+        n_proj_obs = nrow(new_covariate_data)
+        t_proj = new_covariate_data$Year
+        lat_proj = new_covariate_data$Lat
+        lon_proj = new_covariate_data$Lon
         new_covariate_data = new_covariate_data[, match(colnames(x$covariate_data), 
             colnames(new_covariate_data))]
         NN = RANN::nn2(query = x$covariate_data[, c("Lat", "Lon", 
@@ -1877,9 +1872,9 @@ project_model_aja<- function (x, what, n_proj = NULL, n_samples, uncert_res, new
             x$covariate_data = x$covariate_data[-which(NN$nn.dist == 
                 0), , drop = FALSE]
         }
-        if("Season" %in% names(new_covariate_data)){
-          new_covariate_data$Season <- factor(new_covariate_data$Season, 
-            levels = levels(x$covariate_data$Season))
+        if ("Season" %in% names(new_covariate_data)) {
+            new_covariate_data$Season <- factor(new_covariate_data$Season, 
+                levels = levels(x$covariate_data$Season))
         }
     }
     if (is.null(new_catchability_data)) {
@@ -1888,8 +1883,10 @@ project_model_aja<- function (x, what, n_proj = NULL, n_samples, uncert_res, new
         if (!all(colnames(x$catchability_data) %in% colnames(new_catchability_data))) {
             stop("Please ensure that all columns of `x$catchability_data` are present in `new_catchability_data`")
         }
-        new_catchability_data = new_catchability_data[, match(colnames(x$catchability_data), 
-            colnames(new_catchability_data))]
+        new_catchability_data = new_catchability_data[, match(
+            colnames(x$catchability_data),
+            colnames(new_catchability_data)
+        )]
         new_catchability_data = rbind(x$catchability_data, new_catchability_data)
     }
     if (any(x$data_list$RhoConfig %in% c(0, 3))) {
@@ -1909,70 +1906,73 @@ project_model_aja<- function (x, what, n_proj = NULL, n_samples, uncert_res, new
     if (historical_uncertainty == "both") {
         u_zr = rmvnorm_prec(mu = Obj$env$last.par.best, prec = Sdreport$jointPrecision, 
             n.sims = n_samples, seed = seed)
-    } else if (historical_uncertainty == "random") {
+    }
+    else if (historical_uncertainty == "random") {
         Obj$retape()
         Obj$fn(x$parameter_estimates$par)
         u_zr = Obj$env$last.par.best %o% rep(1, n_samples)
         set.seed(seed)
         MC = Obj$env$MC(keep = TRUE, n = n_samples, antithetic = FALSE)
         u_zr[Obj$env$random, ] = attr(MC, "samples")
-    } else if (historical_uncertainty == "none") {
+    }
+    else if (historical_uncertainty == "none") {
         u_zr = Obj$env$last.par.best %o% rep(1, n_samples)
-    } else {
+    }
+    else {
         stop("Check `historical_uncertainty` argument")
     }
-    # t_i = c(x$data_frame$t_i, t_proj)
-    # Lon_i = c(x$data_frame$Lon_i, lon_proj)
-    # Lat_i = c(x$data_frame$Lat_i, lat_proj)
-    # b_i = c(x$data_list$b_i, as_units(sample(c(0, 1), size = n_proj_obs, 
-    #     replace = TRUE), units(x$data_list$b_i)))
-    # v_i = c(x$data_frame$v_i, rep(0, n_proj_obs))
+    # t_i = c(x$data_frame$t_i, max(x$data_frame$t_i) + rep(seq_len(n_proj), 
+    #     each = 2))
+    # b_i = c(x$data_list$b_i, as_units(rep(c(0, mean(x$data_frame$b_i)), 
+    #     n_proj), units(x$data_list$b_i)))
+    # v_i = c(x$data_frame$v_i, rep(0, 2 * n_proj))
+    # Lon_i = c(x$data_frame$Lon_i, rep(mean(x$data_frame$Lon_i), 
+    #     2 * n_proj))
+    # Lat_i = c(x$data_frame$Lat_i, rep(mean(x$data_frame$Lat_i), 
+    #     2 * n_proj))
     # a_i = c(x$data_list$a_i, as_units(rep(mean(x$data_frame$a_i), 
-    #     n_proj_obs), units(fit$data_list$a_i)))
-    # PredTF_i = c(x$data_list$PredTF_i, rep(1, n_proj_obs))
-    # c_iz = rbind(matrix(x$data_list$c_iz), matrix(0, nrow = n_proj_obs))
-
-    t_i = c(x$data_frame$t_i, max(x$data_frame$t_i) + rep(seq_len(n_proj), 
-        each = 2))
-    b_i = c(x$data_list$b_i, as_units(rep(c(0, mean(x$data_frame$b_i)), 
-        n_proj), units(x$data_list$b_i)))
-    v_i = c(x$data_frame$v_i, rep(0, 2 * n_proj))
-    Lon_i = c(x$data_frame$Lon_i, rep(mean(x$data_frame$Lon_i), 
-        2 * n_proj))
-    Lat_i = c(x$data_frame$Lat_i, rep(mean(x$data_frame$Lat_i), 
-        2 * n_proj))
+    #     2 * n_proj), units(x$data_list$a_i)))
+    # PredTF_i = c(x$data_list$PredTF_i, rep(1, 2 * n_proj))
+    # c_iz = rbind(x$data_list$c_iz, x$data_list$c_iz[rep(seq_len(n_proj), 
+    #     each = 2), , drop = FALSE])
+    # new_catchability_data = rbind(x$catchability_data, new_catchability_data[1:486, 
+    #     ])
+    # str(b_i)
+    # str(new_catchability_data)
+    t_i = c(x$data_frame$t_i, t_proj)
+    Lon_i = c(x$data_frame$Lon_i, lon_proj)
+    Lat_i = c(x$data_frame$Lat_i, lat_proj)
+    b_i = c(x$data_list$b_i, as_units(sample(c(0, 1), size = n_proj_obs, 
+        replace = TRUE), units(x$data_list$b_i)))
+    v_i = c(x$data_frame$v_i, rep(0, n_proj_obs))
     a_i = c(x$data_list$a_i, as_units(rep(mean(x$data_frame$a_i), 
-        2 * n_proj), units(x$data_list$a_i)))
-    PredTF_i = c(x$data_list$PredTF_i, rep(1, 2 * n_proj))
-    c_iz = rbind(x$data_list$c_iz, x$data_list$c_iz[rep(seq_len(n_proj), 
-        each = 2), , drop = FALSE])
-    new_catchability_data = rbind(x$catchability_data, x$catchability_data[rep(seq_len(n_proj), 
-        each = 2), , drop = FALSE])
+        n_proj_obs), units(fit$data_list$a_i)))
+    PredTF_i = c(x$data_list$PredTF_i, rep(1, n_proj_obs))
+    c_iz = rbind(matrix(x$data_list$c_iz), matrix(0, nrow = n_proj_obs))
     proj_t = x$data_list$n_t + seq_len(n_proj)
-    if("Season" %in% names(new_covariate_data)) {
-      x1 = fit_model(settings = x$settings, Lat_i = Lat_i, Lon_i = Lon_i, 
-        t_i = t_i, b_i = b_i, a_i = a_i, v_i = v_i, c_iz = c_iz, 
-        PredTF_i = PredTF_i, covariate_data = new_covariate_data, 
-        X1_formula = x$X1_formula, X2_formula = x$X2_formula, 
-        X_contrasts = list(Season = contrasts(new_covariate_data$Season, 
-            contrasts = FALSE)), X1config_cp = x$X1config_cp, 
-        X2config_cp = x$X2config_cp, catchability_data = new_catchability_data, 
-        Q1config_k = x$Q1config_k, Q2config_k = x$Q2config_k, 
-        Q1_formula = x$Q1_formula, Q2_formula = x$Q2_formula, 
-        build_model = FALSE, working_dir = working_dir, input_grid = input_grid)
-    } else {
-      x1 = fit_model(settings = x$settings, Lat_i = Lat_i, Lon_i = Lon_i, 
-        t_i = t_i, b_i = b_i, a_i = a_i, v_i = v_i, c_iz = c_iz, 
-        PredTF_i = PredTF_i, covariate_data = new_covariate_data, 
-        X1_formula = x$X1_formula, X2_formula = x$X2_formula, X1config_cp = x$X1config_cp, 
-        X2config_cp = x$X2config_cp, catchability_data = new_catchability_data, 
-        Q1config_k = x$Q1config_k, Q2config_k = x$Q2config_k, 
-        Q1_formula = x$Q1_formula, Q2_formula = x$Q2_formula, 
-        build_model = FALSE, working_dir = working_dir)
+    if ("Season" %in% names(new_covariate_data)) {
+        x1 = fit_model(settings = x$settings, Lat_i = Lat_i, 
+            Lon_i = Lon_i, t_i = t_i, b_i = b_i, a_i = a_i, v_i = v_i, 
+            c_iz = c_iz, PredTF_i = PredTF_i, covariate_data = new_covariate_data, 
+            X1_formula = x$X1_formula, X2_formula = x$X2_formula, 
+            X_contrasts = list(Season = contrasts(new_covariate_data$Season, 
+                contrasts = FALSE)), X1config_cp = x$X1config_cp, 
+            X2config_cp = x$X2config_cp, catchability_data = new_catchability_data, 
+            Q1config_k = x$Q1config_k, Q2config_k = x$Q2config_k, 
+            Q1_formula = x$Q1_formula, Q2_formula = x$Q2_formula, 
+            build_model = FALSE, working_dir = working_dir, input_grid = input_grid, extrapolation_list = extrapolation_list, spatial_list = proj_spatial)
     }
-    
+    else {
+        x1 = fit_model(settings = x$settings, Lat_i = Lat_i, 
+            Lon_i = Lon_i, t_i = t_i, b_i = b_i, a_i = a_i, v_i = v_i, 
+            c_iz = c_iz, PredTF_i = PredTF_i, covariate_data = new_covariate_data, 
+            X1_formula = x$X1_formula, X2_formula = x$X2_formula, 
+            X1config_cp = x$X1config_cp, X2config_cp = x$X2config_cp, 
+            catchability_data = new_catchability_data, Q1config_k = x$Q1config_k, 
+            Q2config_k = x$Q2config_k, Q1_formula = x$Q1_formula, 
+            Q2_formula = x$Q2_formula, build_model = FALSE, working_dir = working_dir)
+    }
     out = vector("list", length = n_samples)
-  
     for (sampleI in seq_len(n_samples)) {
         ParList1 = x1$tmb_list$Parameters
         ParList = Obj$env$parList(par = u_zr[, sampleI])
@@ -2011,29 +2011,30 @@ project_model_aja<- function (x, what, n_proj = NULL, n_samples, uncert_res, new
             ParList1$beta2_ft = ifelse(is.na(tmp), rowMeans(tmp, 
                 na.rm = TRUE) %o% rep(1, ncol(tmp)), ParList1$beta2_ft)
         }
-        if("Season" %in% names(new_covariate_data)){
-           x2 = fit_model(settings = x$settings, Lat_i = Lat_i, 
-            Lon_i = Lon_i, t_i = t_i, b_i = b_i, a_i = a_i, v_i = v_i, 
-            c_iz = c_iz, PredTF_i = PredTF_i, covariate_data = new_covariate_data, 
-            X1_formula = x$X1_formula, X2_formula = x$X2_formula, 
-            X1config_cp = x$X1config_cp, X2config_cp = x$X2config_cp, 
-            X_contrasts = list(Season = contrasts(new_covariate_data$Season, 
-                contrasts = FALSE)), catchability_data = new_catchability_data, 
-            Q1config_k = x$Q1config_k, Q2config_k = x$Q2config_k, 
-            Q1_formula = x$Q1_formula, Q2_formula = x$Q2_formula, 
-            run_model = FALSE, Parameters = ParList1, working_dir = working_dir, 
-            input_grid = input_grid)
-        } else {
-           x2 = fit_model(settings = x$settings, Lat_i = Lat_i, 
-            Lon_i = Lon_i, t_i = t_i, b_i = b_i, a_i = a_i, v_i = v_i, 
-            c_iz = c_iz, PredTF_i = PredTF_i, covariate_data = new_covariate_data, 
-            X1_formula = x$X1_formula, X2_formula = x$X2_formula, 
-            X1config_cp = x$X1config_cp, X2config_cp = x$X2config_cp, catchability_data = new_catchability_data, 
-            Q1config_k = x$Q1config_k, Q2config_k = x$Q2config_k, 
-            Q1_formula = x$Q1_formula, Q2_formula = x$Q2_formula, 
-            run_model = FALSE, Parameters = ParList1, working_dir = working_dir)
+        if ("Season" %in% names(new_covariate_data)) {
+            x2 = fit_model(settings = x$settings, Lat_i = Lat_i, 
+                Lon_i = Lon_i, t_i = t_i, b_i = b_i, a_i = a_i, 
+                v_i = v_i, c_iz = c_iz, PredTF_i = PredTF_i, 
+                covariate_data = new_covariate_data, X1_formula = x$X1_formula, 
+                X2_formula = x$X2_formula, X1config_cp = x$X1config_cp, 
+                X2config_cp = x$X2config_cp, X_contrasts = list(Season = contrasts(new_covariate_data$Season, 
+                  contrasts = FALSE)), catchability_data = new_catchability_data, 
+                Q1config_k = x$Q1config_k, Q2config_k = x$Q2config_k, 
+                Q1_formula = x$Q1_formula, Q2_formula = x$Q2_formula, 
+                run_model = FALSE, Parameters = ParList1, working_dir = working_dir, 
+                input_grid = input_grid, extrapolation_list = extrapolation_list, spatial_list = proj_spatial)
         }
-       
+        else {
+            x2 = fit_model(settings = x$settings, Lat_i = Lat_i, 
+                Lon_i = Lon_i, t_i = t_i, b_i = b_i, a_i = a_i, 
+                v_i = v_i, c_iz = c_iz, PredTF_i = PredTF_i, 
+                covariate_data = new_covariate_data, X1_formula = x$X1_formula, 
+                X2_formula = x$X2_formula, X1config_cp = x$X1config_cp, 
+                X2config_cp = x$X2config_cp, catchability_data = new_catchability_data, 
+                Q1config_k = x$Q1config_k, Q2config_k = x$Q2config_k, 
+                Q1_formula = x$Q1_formula, Q2_formula = x$Q2_formula, 
+                run_model = FALSE, Parameters = ParList1, working_dir = working_dir)
+        }
         x2$tmb_list$Obj$env$data$Options_list$simulate_t[] = c(rep(0, 
             x$data_list$n_t), rep(1, n_proj))
         out[[sampleI]] <- simulate_data(fit = x2, type = 1, random_seed = NULL)
@@ -2050,6 +2051,7 @@ project_model_aja<- function (x, what, n_proj = NULL, n_samples, uncert_res, new
     }
     return(out)
 }
+
 
 # predict.fit_model_aja <- function(x, what = "D_i", Lat_i, Lon_i, t_i, a_i, c_iz = rep(0, length(t_i)), v_i = rep(0, length(t_i)), new_covariate_data = NULL, new_catchability_data = NULL, do_checks = TRUE, working_dir = paste0(getwd(), "/")) {
 #   if (FALSE) {
